bca6d1d688a230fa68f5d7f72a3ebba543200b47
Yalkom <jccuevasm76@gmail.com>
2015-10-29 13:15:00 +0100

    Búsqueda dispositivos BT
    Añadido la búsqueda para dispositivos BT

--- Examples/src/es/uja/git/sv/examples/ConnectivityBT.java
+++ Examples/src/es/uja/git/sv/examples/ConnectivityBT.java
@@ -2,13 +2,17 @@ package es.uja.git.sv.examples;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,10 +22,11 @@ public final class ConnectivityBT extends Activity {
 	private static final String TAG = "Bluetooth";
 	private static final int REQUEST_ENABLE_BT = 0;
 
-
 	private TextView mTextStatus;
 	private BluetoothAdapter mBluetoothAdapter;
-	private boolean mIsBTEnabled=false;
+	private boolean mIsBTEnabled = false;
+	private BroadcastReceiver mReceiver = null;
+	private ArrayAdapter<String> mArrayAdapter = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,12 +35,14 @@ public final class ConnectivityBT extends Activity {
 
 		// Setup UI
 		mTextStatus = (TextView) findViewById(R.id.bluetooth_textView_result);
-
+		mArrayAdapter = new ArrayAdapter<String>(this, 0);
 		// Setup Bluetooth
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		if (mBluetoothAdapter == null) {
 			// Device does not support Bluetooth
-			Toast.makeText(this, getString(R.string.connectivity_bt_notsupported), Toast.LENGTH_LONG).show();
+			Toast.makeText(this,
+					getString(R.string.connectivity_bt_notsupported),
+					Toast.LENGTH_LONG).show();
 
 			setResult(RESULT_CANCELED);
 			finish();
@@ -43,17 +50,18 @@ public final class ConnectivityBT extends Activity {
 		}
 
 		checkBT();
+
 	}
 
 	protected void checkBT() {
 		// Se comprueba si BT es activo
 		if (!mBluetoothAdapter.isEnabled()) {
-			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 		}
 	}
 
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu_conectividad_bt, menu);
@@ -65,11 +73,16 @@ public final class ConnectivityBT extends Activity {
 		switch (item.getItemId()) {
 
 		case R.id.menu_searchBT:
-			Toast.makeText(this, this.getResources().getString(R.string.bluetooth_scanningdevices), Toast.LENGTH_LONG)
-					.show();
+			Toast.makeText(
+					this,
+					this.getResources().getString(
+							R.string.bluetooth_scanningdevices),
+					Toast.LENGTH_LONG).show();
 
-			Log.d(TAG, "onClick() bluetooth look for devices");
-			
+			if (this.mIsBTEnabled && mReceiver != null) {
+				mBluetoothAdapter.startDiscovery();
+				Log.d(TAG, "onClick() bluetooth look for devices");
+			}
 			return true;
 		}
 
@@ -78,13 +91,42 @@ public final class ConnectivityBT extends Activity {
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		switch(requestCode)
-		{
+		switch (requestCode) {
 		case REQUEST_ENABLE_BT:
-			if(resultCode==RESULT_OK)
-				this.mIsBTEnabled=true;
-			else
-				this.mIsBTEnabled=false;
+			if (resultCode == RESULT_OK) {
+				this.mIsBTEnabled = true;
+
+				// Si el BT se activa se crea un BroadcastReceiver para
+				// ACTION_FOUND
+				mReceiver = new BroadcastReceiver() {
+					public void onReceive(Context context, Intent intent) {
+						String action = intent.getAction();
+						// When discovery finds a device
+						if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+							// Get the BluetoothDevice object from the Intent
+							BluetoothDevice device = intent
+									.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+							// Add the name and address to an array adapter to
+							// show in a ListView
+							mArrayAdapter.add(device.getName() + "\n"
+									+ device.getAddress());
+							Toast.makeText(
+									getApplicationContext(),
+									device.getName() + "\n"
+											+ device.getAddress(),
+									Toast.LENGTH_LONG).show();
+						}
+					}
+
+				};
+				// Register the BroadcastReceiver
+				IntentFilter filter = new IntentFilter(
+						BluetoothDevice.ACTION_FOUND);
+				registerReceiver(mReceiver, filter); // Don't forget to
+														// unregister during
+														// onDestroy
+			} else
+				this.mIsBTEnabled = false;
 			break;
 		}
 		super.onActivityResult(requestCode, resultCode, data);
