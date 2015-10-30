45ea7e2d04db1be4f7caf88034aeeac91744528a
Juan Carlos Cuevas Martínez <jccuevas@ujaen.es>
2015-10-30 09:11:55 +0100

    Comunicación BT establecidad
    Creada la hebra servidora
    Solucionado el problema de los handler (Looper.prepare())
    Parece que hay comunicación, pero los datos no se formatean bien

--- Examples/src/es/uja/git/sv/examples/ConnectivityBT.java
+++ Examples/src/es/uja/git/sv/examples/ConnectivityBT.java
@@ -12,6 +12,7 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -19,6 +20,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
@@ -100,7 +102,7 @@ public final class ConnectivityBT extends Activity {
 					// show in a ListView
 
 					mDeviceList.add(device);
-					
+
 					mArrayAdapter.add(device.getName() + "\n" + device.getAddress() + "  RSSI: " + rssi + "dBm");
 					mList.setAdapter(mArrayAdapter);
 
@@ -114,6 +116,13 @@ public final class ConnectivityBT extends Activity {
 												// unregister during
 												// onDestroy
 
+	}
+
+	@Override
+	protected void onDestroy() {
+		if (mReceiver != null)
+			unregisterReceiver(mReceiver);
+		super.onDestroy();
 	}
 
 	protected void checkBT() {
@@ -147,24 +156,33 @@ public final class ConnectivityBT extends Activity {
 				Log.d(TAG, "menu buscar - Buscando dispositivos");
 			}
 			return true;
-		case R.id.menu_startBTserver:
+		case R.id.menu_connectBT:
 			if (this.mIsBTEnabled && mReceiver != null) {
-				
+
 				Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
 				// If there are paired devices
 				if (pairedDevices.size() > 0) {
-				    // Loop through paired devices
-				    for (BluetoothDevice device : pairedDevices) {
-				        // Add the name and address to an array adapter to show in a ListView
-				    	Log.d(TAG, "menu play - iniciando conexión con "+device.getAddress()+" "+device.getName());
-				    	new ConnectThread(device).start();;
-				        //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-				    }
+					// Loop through paired devices
+					for (BluetoothDevice device : pairedDevices) {
+						// Add the name and address to an array adapter to show
+						// in a ListView
+						Log.d(TAG,
+								"menu play - iniciando conexión con " + device.getAddress() + " " + device.getName());
+						new ConnectThread(device).start();
+						;
+						// mArrayAdapter.add(device.getName() + "\n" +
+						// device.getAddress());
+					}
 				}
-					
 
-					
-				
+			}
+
+			return true;
+		case R.id.menu_startBTserver:
+			if (this.mIsBTEnabled && mReceiver != null) {
+
+				new AcceptThread().start();
+
 			}
 
 			return true;
@@ -240,6 +258,52 @@ public final class ConnectivityBT extends Activity {
 		}
 	}
 
+	private class AcceptThread extends Thread {
+		private final BluetoothServerSocket mmServerSocket;
+		private final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+		private final String SERVICE_NAME = "BTDATA";
+
+		public AcceptThread() {
+			// Use a temporary object that is later assigned to mmServerSocket,
+			// because mmServerSocket is final
+			BluetoothServerSocket tmp = null;
+			try {
+				// MY_UUID is the app's UUID string, also used by the client
+				// code
+				tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord(SERVICE_NAME, MY_UUID);
+			} catch (IOException e) {
+			}
+			mmServerSocket = tmp;
+		}
+
+		public void run() {
+			BluetoothSocket socket = null;
+			// Keep listening until exception occurs or a socket is returned
+			while (true) {
+				try {
+					socket = mmServerSocket.accept();
+				} catch (IOException e) {
+					break;
+				}
+				// If a connection was accepted
+				while (socket != null) {
+					// Do work to manage the connection (in a separate thread)
+					new ServerThread(socket).start();
+					// mmServerSocket.close();
+					break;
+				}
+			}
+		}
+
+		/** Will cancel the listening socket, and cause the thread to finish */
+		public void cancel() {
+			try {
+				mmServerSocket.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+
 	private class ConnectedThread extends Thread {
 		private final BluetoothSocket mmSocket;
 		private final InputStream mmInStream;
@@ -263,26 +327,123 @@ public final class ConnectivityBT extends Activity {
 		}
 
 		public void run() {
-			byte[] buffer = new String("Mensaje Bluetooth desde android").getBytes(); // buffer store for the stream
+			byte[] buffer = new String("Mensaje Bluetooth desde android").getBytes(); 
+			int bytes; // bytes returned from read()
+
+			Looper.prepare();
+			
+			
+			// Keep listening to the InputStream until an exception occurs
+			// while (true) {
+			try {
+
+				mmInStream.read(buffer);
+
+				Message msgObj = mHandler.obtainMessage();
+				msgObj.what = ConnectivityBT.MESSAGE_READ;
+				Bundle b = new Bundle();
+				b.putByteArray(MESSAGE_KEY_DATAREAD, buffer);
+				msgObj.setData(b);
+				mHandler.sendMessage(msgObj);
+
+				Toast.makeText(getApplicationContext(), new String(buffer), Toast.LENGTH_LONG).show();
+				//
+				buffer = new String("Mensaje Bluetooth desde un cliente android").getBytes(); // buffer
+																								// store
+																								// for
+																								// the
+																								// stream
+				mmOutStream.write(buffer);
+				// Send the obtained bytes to the UI activity
+
+			} catch (IOException e) {
+				Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+			}
+			// }
+			try {
+				mmSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		/* Call this from the main activity to send data to the remote device */
+		public void write(byte[] bytes) {
+			try {
+				mmOutStream.write(bytes);
+			} catch (IOException e) {
+			}
+		}
+
+		/* Call this from the main activity to shutdown the connection */
+		public void cancel() {
+			try {
+				mmSocket.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+
+	private class ServerThread extends Thread {
+		private final BluetoothSocket mmSocket;
+		private final InputStream mmInStream;
+		private final OutputStream mmOutStream;
+
+		public ServerThread(BluetoothSocket socket) {
+			mmSocket = socket;
+			InputStream tmpIn = null;
+			OutputStream tmpOut = null;
+
+			// Get the input and output streams, using temp objects because
+			// member streams are final
+			try {
+				tmpIn = socket.getInputStream();
+				tmpOut = socket.getOutputStream();
+			} catch (IOException e) {
+				
+			}
+
+			mmInStream = tmpIn;
+			mmOutStream = tmpOut;
+		}
+
+		public void run() {
+			byte[] buffer = new String("Bienvenido Bluetooth desde android").getBytes(); // buffer
+																							// store
+																							// for
+			Looper.prepare();																			// the
+																							// stream
 			int bytes; // bytes returned from read()
 
 			// Keep listening to the InputStream until an exception occurs
-			while (true) {
-				try {
-					// Read from the InputStream
-					mmOutStream.write(buffer);
-					// Send the obtained bytes to the UI activity
+			// while (true) {
+			try {
+				// Read from the InputStream
+				mmOutStream.write(buffer);
+				// Send the obtained bytes to the UI activity
 
-					Message msgObj = mHandler.obtainMessage();
-					msgObj.what = ConnectivityBT.MESSAGE_READ;
-					Bundle b = new Bundle();
-					b.putByteArray(MESSAGE_KEY_DATAREAD, buffer);
-					msgObj.setData(b);
-					mHandler.sendMessage(msgObj);
+				mmInStream.read(buffer);
 
-				} catch (IOException e) {
-					break;
-				}
+				Message msgObj = mHandler.obtainMessage();
+				msgObj.what = ConnectivityBT.MESSAGE_READ;
+				Bundle b = new Bundle();
+				b.putByteArray(MESSAGE_KEY_DATAREAD, buffer);
+				msgObj.setData(b);
+				mHandler.sendMessage(msgObj);
+
+				Toast.makeText(getApplicationContext(), new String(buffer), Toast.LENGTH_LONG).show();
+
+			} catch (IOException e) {
+
+				Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+			}
+			// }
+			try {
+				mmSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 
